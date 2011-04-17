8f7bd6431e43dae21819c848aa9ad0d21ecb898a
Andrii Kurinnyi <andrew@marpasoft.com>
2011-04-17 01:21:14 +0000

    Added staticfiles urls, index template

--- chatsite/urls.py
+++ chatsite/urls.py
@@ -1,10 +1,21 @@
+from django.conf import settings
 from django.conf.urls.defaults import patterns, include, url
+from django.views.generic import TemplateView
 
 from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
+    url(r'^$', TemplateView.as_view(template_name="index.html"), name="root_url"),
     url(r'^hookbox/', include('chatbox.urls')),
-
     url(r'^admin/', include(admin.site.urls)),
 )
+
+if settings.DEBUG:
+    urlpatterns += patterns('',
+        (r'^site_media/media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT}),
+    )
+
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
+urlpatterns += staticfiles_urlpatterns()
