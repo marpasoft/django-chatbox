e2558f51a0e4b25d9e76dcaf346cab3e887ff9ab
Andrii Kurinnyi <andrew@marpasoft.com>
2011-04-18 07:24:26 +0000

    Added some styling to chat

---
+++ chatsite/sass/config.rb
@@ -0,0 +1,11 @@
+# Require any additional compass plugins here.
+# Set this to the root of your project when deployed:
+http_path = "/"
+css_dir = "../static/css"
+sass_dir = "src"
+images_dir = "../static/img"
+javascripts_dir = "javascripts"
+# To enable relative paths to assets via compass helper functions. Uncomment:
+# relative_assets = true
+preferred_syntax = :sass
+line_comments = false
