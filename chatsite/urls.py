90ea2c9242786b4d080c1bb20d5230e799897e98
Andrii Kurinnyi <andrew@marpasoft.com>
2011-04-18 08:58:06 +0000

    Require login to chatbox

--- chatsite/urls.py
+++ chatsite/urls.py
@@ -2,11 +2,12 @@ from django.conf import settings
 from django.conf.urls.defaults import patterns, include, url
 from django.views.generic import TemplateView
 
+from django.contrib.auth.decorators import login_required
 from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    url(r'^$', TemplateView.as_view(template_name="index.html"), name="root_url"),
+    url(r'^$', login_required(TemplateView.as_view(template_name="index.html")), name="root_url"),
     url(r'^hookbox/', include('chatbox.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
